cbd85ec6bc781ccb025bd6c8e7157147e185b8f7
Jorge Ram <44329543+josejorgerami@users.noreply.github.com>
2025-05-17 17:06:49 -0600

    Create ArtJR.kt

---
+++ 1-introduction-to-kotlin/ArtJR.kt
@@ -0,0 +1,21 @@
+// ASCII Art project Jorge Ram
+/*
+
+          +       + 
+         + +     + +
+        +   +_ _+   +
+       +             + 
+      +    0    0     +
+ =====      (*)        =====      
+======                 ======  
+
+*/
+fun main() {
+  println(" ".repeat(10) + "+" + " ".repeat(7) + "+\n" + 
+  " ".repeat(9) + "+ +" + " ".repeat(5) + "+ +\n" + 
+  " ".repeat(8) + "+" + " ".repeat(3) + "+___+" + " ".repeat(3) + "+\n" +
+  " ".repeat(7) + "+" + " ".repeat(13) + "+\n" +
+  " ".repeat(6) + "+" + " ".repeat(5) + "0" + " ".repeat(3) + "0" + " ".repeat(5) + "+\n" +
+  " " + "=".repeat(5) + " ".repeat(7) + "(*)" + " ".repeat(7) + "=".repeat(5) +"\n" +
+  "=".repeat(6) + " ".repeat(17) + "=".repeat(6)   )
+}
